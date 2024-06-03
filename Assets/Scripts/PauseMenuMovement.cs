892efa1e99dff59eafc3ccc8324cad230b2ed35c
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-06-02 19:04:56 -0700

    Translate with no slide

--- Assets/Scripts/PauseMenuMovement.cs
+++ Assets/Scripts/PauseMenuMovement.cs
@@ -1,29 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PauseMenuMovement : MonoBehaviour
 {
     //Variables
     public float MoveSpeed = 30f;
-    public float StoppingDistance = 13f;
     public Rigidbody2D rb;
+    bool Paused;
+    bool Moving;
     Vector2 Movement;
+
+    void Start()
+    {
+        Movement = new Vector2(0, -1);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Movement = new Vector2(0, -1);
-        Move();
-    }
-
-    void Move()
-    {
-        Vector2 v = new Vector2(Movement.x, Movement.y);
-        if (gameObject.transform.position.y > StoppingDistance) {
-            rb.AddForce(v.normalized * MoveSpeed);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                UnpauseGame();
+                Vector2 v = new Vector2(Movement.x, -Movement.y);
+                Move(v);
+            }
+            else
+            {
+                PauseGame();
+                Vector2 v = new Vector2(Movement.x, Movement.y);
+                Move(v);
+            }
         }
     }
 
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Move(Vector2 v)
+    {
+        rb.transform.Translate(v * MoveSpeed);
+    }
 }
