3b14dac29fab0c512c395d214e68f112ffbdbd01
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-06-06 17:36:13 -0700

    Sliding pause menu working with lerp ! ! ! :D

--- Assets/Scripts/PauseMenuMovement.cs
+++ Assets/Scripts/PauseMenuMovement.cs
@@ -8,16 +8,11 @@ using UnityEngine.AI;
 public class PauseMenuMovement : MonoBehaviour
 {
     //Variables
-    public float MoveSpeed = 30f;
-    public Rigidbody2D rb;
-    bool Paused;
-    bool Moving;
-    Vector2 Movement;
-
-    void Start()
-    {
-        Movement = new Vector2(0, -1);
-    }
+    public Transform TargetDown;
+    public Transform TargetUp;
+    public Transform Target;
+    public float Velocity;
+    public bool Paused;
 
     // Update is called once per frame
     void Update()
@@ -27,32 +22,39 @@ public class PauseMenuMovement : MonoBehaviour
             if (Paused)
             {
                 UnpauseGame();
-                Vector2 v = new Vector2(Movement.x, -Movement.y);
-                Move(v);
             }
             else
             {
                 PauseGame();
-                Vector2 v = new Vector2(Movement.x, Movement.y);
-                Move(v);
             }
         }
+        Move();
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;        
+        Target = TargetUp;
+        // PauseMenu.SetActive(false);
+        Paused = false;
     }
 
     public void PauseGame()
     {
         Time.timeScale = 0f;
+        Target = TargetDown;
+        // PauseMenu.SetActive(true);
         Paused = true;
     }
 
-    public void UnpauseGame()
+    void Move()
     {
-        Time.timeScale = 1f;
-        Paused = false;
-    }
-
-    void Move(Vector2 v)
-    {
-        rb.transform.Translate(v * MoveSpeed);
+        transform.position = Vector2.Lerp(transform.position, Target.position, Velocity * Time.unscaledDeltaTime);
     }
 }
+
+// Smooth towards the target
+
+    // void Update() {
+    //     transform.position = Vector2.Lerp(transform.position, target.position, Velocity * Time.deltaTime);
+    // }
