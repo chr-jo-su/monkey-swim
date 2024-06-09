a67a05ca1e1b753036b9a038bf1f506578742c76
ParallelVoid <86805807+ParallelVoid@users.noreply.github.com>
2024-06-09 12:49:29 -0700

    Fish Follow Player

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -10,10 +10,14 @@ public class fishMovement : MonoBehaviour
     private float theta = 0f;
     private float thetaStep = Mathf.PI / 32f;
     Vector2 movement;
+    public bool following = false;
+    public float distance;
+    private Transform player;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
@@ -26,20 +30,32 @@ public class fishMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        theta += thetaStep; //*moveSpeed
+        distance = Vector2.Distance(transform.position, player.transform.position);
 
-        //Movement
-        rb.MovePosition(rb.position + movement*moveSpeed*Time.fixedDeltaTime);
-        transform.Rotate(Vector3.forward * Mathf.Sin(theta));
-
-
-        //Change Directions; -8 and 8 are arbitrary edges of the screen; should be reworked later
-        if ((transform.position.x >= 8 && moveSpeed > 0) | (transform.position.x <= -8 && moveSpeed < 0))
-        {
-            moveSpeed *= -1;
-            gameObject.transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        if (following) {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
         }
 
+        else {
+            theta += thetaStep; //*moveSpeed
+
+            //Movement
+            rb.MovePosition(rb.position + movement*moveSpeed*Time.fixedDeltaTime);
+            transform.Rotate(Vector3.forward * Mathf.Sin(theta));
+
+
+            //Change Directions; -8 and 8 are arbitrary edges of the screen; should be reworked later
+            if ((transform.position.x >= 8 && moveSpeed > 0) | (transform.position.x <= -8 && moveSpeed < 0))
+            {
+                moveSpeed *= -1;
+                gameObject.transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            }
+
+            if (distance < 7){
+                following = true;
+            }
+
+        }
     }
 
 }
