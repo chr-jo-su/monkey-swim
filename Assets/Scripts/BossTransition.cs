30cfd39b2f95c18a71685c8f9519f669583fde7a
Timothy Solnyshkin <timothy.solnyshkin@gmail.com>
2024-12-24 22:30:46 -0800

    Transition to Boss Fight

--- Assets/Scripts/BossTransition.cs
+++ Assets/Scripts/BossTransition.cs
@@ -8,11 +8,16 @@ public class BossTransition : MonoBehaviour
     public Canvas popUp;
     public Button yesButton;
     private GameObject playerObject;
+    public Camera  cam1;
+    public Camera  cam2;
+
 
     public void Start()
     {
         popUp.enabled = false;
         yesButton.onClick.AddListener(teleportPlayer);
+        cam1.enabled = true;
+        cam2.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,8 +34,11 @@ public class BossTransition : MonoBehaviour
     public void teleportPlayer()
     {
         //StartCoroutine(ShowTransition());
-        playerObject.transform.position = new Vector2(100, 100);
+        playerObject.transform.position = new Vector2(120, 5);
         popUp.enabled = false;
+
+            cam1.enabled = false;
+            cam2.enabled = true;
     }
 
     //private IEnumerator ShowTransition()
