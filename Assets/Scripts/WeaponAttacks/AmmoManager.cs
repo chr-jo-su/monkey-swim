cb1ba372d1beeeceb54ce1d27a659090c54821d7
Jimmy Rashid <jimmyrashid22@gmail.com>
2025-01-21 20:58:41 -0800

    Slight change to script + rearranged file location

--- Assets/Scripts/WeaponAttacks/AmmoManager.cs
+++ Assets/Scripts/WeaponAttacks/AmmoManager.cs
@@ -7,7 +7,7 @@ public class AmmoManager : MonoBehaviour
     public GameObject One;
     public GameObject Two;
     public GameObject Three;
-    
+
     private WeaponManager Script;
     private float Count;
 
@@ -18,25 +18,25 @@ public class AmmoManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         Count = Script.BananarangCount;
 
-        if (Count == 3) {
-            One.SetActive(false);
-            Two.SetActive(false);
+        One.SetActive(false);
+        Two.SetActive(false);
+        Three.SetActive(false);
+
+        if (Count == 3)
+        {
             Three.SetActive(true);
-        } else if (Count == 2) {
-            One.SetActive(false);
-            Two.SetActive(true);
-            Three.SetActive(false);
-        } else if (Count == 1) {
-            One.SetActive(true);
-            Two.SetActive(false);
-            Three.SetActive(false);
-        } else {
-            One.SetActive(false);
-            Two.SetActive(false);
-            Three.SetActive(false);
         }
-     }
+        else if (Count == 2)
+        {
+            Two.SetActive(true);
+        }
+        else if (Count == 1)
+        {
+            One.SetActive(true);
+        }
+    }
 }
