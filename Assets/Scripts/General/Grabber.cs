ffba1894984105d7032f8424fa3f31046d5122e3
Sooraj Sannabhadti <2005sooraj@gmail.com>
2025-04-05 02:28:43 -0700

    Fixed bananarang count and grabber issues

--- Assets/Scripts/General/Grabber.cs
+++ Assets/Scripts/General/Grabber.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 
 public class Grabber : MonoBehaviour
 {
-    private GameObject Player;
     [SerializeField] private float velocity = 2f;
     public bool goGrabbaGrabba;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Player = GameObject.FindGameObjectWithTag("Player");
-    }
 
     // Update is called once per frame
     void Update()
@@ -26,6 +19,7 @@ public class Grabber : MonoBehaviour
 
     void Grab()
     {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
         gameObject.transform.localPosition = Vector3.Lerp(gameObject.transform.localPosition, Player.transform.position + new Vector3(0, 3.5f, 0), velocity * Time.unscaledDeltaTime);
     }
 
